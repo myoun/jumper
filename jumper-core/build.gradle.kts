b2a65967b033dc8ad802ad9bd03efb2ab708c4f6
myoun <aiden080605@gmail.com>
2022-01-29 00:32:44 +0900

    Dokka & Readme

--- jumper-core/build.gradle.kts
+++ jumper-core/build.gradle.kts
@@ -1,3 +1,21 @@
+plugins {
+    id("org.jetbrains.dokka") version "1.6.10"
+}
+
 dependencies {
     implementation("com.github.shynixn.structureblocklib:structureblocklib-bukkit-api:2.3.0")
+}
+
+tasks {
+    register<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+
+    register<Jar>("dokkaJar") {
+        archiveClassifier.set("javadoc")
+        dependsOn("dokkaHtml")
+
+        from("$buildDir/dokka/html/")
+    }
 }
